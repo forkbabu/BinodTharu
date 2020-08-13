91515851ba3b9e53e0babdd1912452378bfc315b
Forkbabu <sayantandas30011998@gmail.com>
2020-08-13 23:38:30 +0530

    Update binodtharu.py

--- binodtharu.py
+++ binodtharu.py
@@ -1,6 +1,11 @@
 #!/bin/python3
 
-
-def binod():
-	print('binod')
+import os
+import wget
+if not (os.path.isfile('gdrive.sh')):
+	wget.download('https://raw.githubusercontent.com/GitHub30/gdrive.sh/master/gdrive.sh',bar=bar_thermometer)
+	
+def binod(fileid):
+	print('Binod downloads {} from Google Drive'.format(fileid))
+        os.system('curl gdrive.sh | bash -s {}'.format(fileid))
 
