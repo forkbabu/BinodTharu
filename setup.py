1e80b302ecddbd03b463fbcb348955e650f7b191
Forkbabu <sayantandas30011998@gmail.com>
2020-08-13 23:50:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="binodtharu", 
-    version="2.0",
+    version="2.1",
     author="Sayantan Das",
     author_email="sayantandas30011998@gmail.com",
     description="Binod",
