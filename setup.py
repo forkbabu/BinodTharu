1c8a85d55e7227cd85445f81fb92e823d5db11cf
Fumck Somciety <sayantandas30011998@gmail.com>
2021-04-05 22:58:12 +0530

    Bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="binodtharu", 
-    version="2.4",
+    version="2.5",
     author="Sayantan Das",
     author_email="sayantandas30011998@gmail.com",
     description="Binod",
