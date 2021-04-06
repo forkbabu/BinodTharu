0d67e27bcc4b39c8b997f2d55b483feea088a79c
Fumck Somciety <sayantandas30011998@gmail.com>
2021-04-06 14:12:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="binodtharu", 
-    version="2.7",
+    version="2.8",
     author="Sayantan Das",
     author_email="sayantandas30011998@gmail.com",
     description="Binod",
